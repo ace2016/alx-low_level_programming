1b316134c62d98481b91cf5e52373a7429b6a660
Owhofasa Mobo <owhofasam@gmail.com>
2022-09-30 14:48:35 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed
+ * @argc: argument counter
+ * @argv: array of pointer to argument strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
